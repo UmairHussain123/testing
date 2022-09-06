9ef1b890d81f05886e5691723b4433285a306b78
UmairHussain123 <33773362+UmairHussain123@users.noreply.github.com>
2022-09-06 16:24:34 +0500

    Update App.js

--- App.js
+++ App.js
@@ -2,27 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-      <Text>App</Text>
-      <Text>App</Text>
-      <Text>App</Text>
-      <Text>App</Text>
-      <Text>App</Text>
-      <Text>App</Text>
-      <Text>App</Text>
-      <Text>App</Text> <Text>App</Text>
-      <Text>App</Text>
-      <Text>App</Text> <Text>App</Text>
-      <Text>App</Text>
-      <Text>App</Text> <Text>App</Text>
-      <Text>App</Text>
-      <Text>App</Text>
-      <View>aklsjdfaklshflkashflhaslkhfalkjshkl</View>
-      <View>aklsjdfaklshflkashflhaslkhfalkjshkl</View>
-    </View>
-  );
+  return <View></View>;
 };
 
 export default App;
